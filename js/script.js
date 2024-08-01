e6959c28239995212612289c0429151273439cf5
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-08-01 23:03:01 +0700

    Adding Fade in and Fade out animation
    adding animation to main named main-fade like fade-in from right to left
    (right position)
    adding transition to sign-up and tos classes
    (maybe need changes)

--- js/script.js
+++ js/script.js
@@ -4,4 +4,34 @@ const setTheme = () => {
   root.className = newTheme;
 }
 
+const signUp = document.querySelector('.sign-up');
+const tos = document.querySelector('.tos');
+
+const section = () => {
+  if(signUp.className === 'sign-up'){
+    signUp.classList.toggle('show');
+  }
+}
+
+const signUpButton = document.getElementById('sign-up-btn');
+const signUpBackButton = document.getElementById('tos-back');
+
+signUpButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if(signUp.className === 'sign-up show' && tos.className === 'tos'){
+    signUp.classList.toggle('show');
+    signUp.classList.toggle('hide');
+    tos.classList.toggle('show');
+  }
+})
+
+signUpBackButton.addEventListener('click', (e) => {
+  if(signUp.className === 'sign-up hide' && tos.className === 'tos show'){
+    signUp.classList.toggle('show');
+    signUp.classList.toggle('hide');
+    tos.classList.toggle('show');
+  }
+})
+
 document.addEventListener('DOMContentLoaded', setTheme);
+document.addEventListener('DOMContentLoaded', section);
