9abffb853514025b7c75895bd19c51ac81b74031
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-08-01 19:21:44 +0700

    Adding Styles and HTML Structure to index.html
    adding styles and html Structures to index.html
    -header
    -sign-up section
    -terms of service section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+const setTheme = () => {
+  const root = document.documentElement;
+  const newTheme = root.className === 'light' ? 'dark' : 'light';
+  root.className = newTheme;
+}
+
+document.addEventListener('DOMContentLoaded', setTheme);
